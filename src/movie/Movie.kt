1b2bb7090d559ba02117d53ac0fca8b851464276
Mahesh <biradar.m2@gmail.com>
2018-06-24 09:02:57 +0530

    use companion object with private constructor

--- src/movie/Movie.kt
+++ src/movie/Movie.kt
@@ -3,14 +3,45 @@ package movie
 import java.io.File
 import java.time.LocalDate
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.stream.Collectors
 
 /**
  * Created by Mahesh on 23-06-2018.
  */
-class Movie (val imdbID:String, val title:String, val releaseDate: LocalDate, val genres:List<String>
+
+
+enum class Genres() {
+    Comedy,
+    Drama,
+    Action,
+    Musical,
+    Family,
+    Adventure,
+    History,
+    Romance;
+}
+
+class Movie private constructor(val imdbID:String, val title:String, val releaseDate: LocalDate, val genres:List<Genres>
              , val director:String, val actors:List<String>, val actress:List<String>, val duration:Long){
+
+
+
+    companion object factory{
+
+        fun createMovie(record:String) :Movie{
+         val   movieAttributes=record.split(",")
+           return Movie(title = movieAttributes[0],
+                releaseDate = LocalDate.parse(movieAttributes[1], DateTimeFormatter.ofPattern("yyyy/MM/dd")),
+                actors = movieAttributes[2].split("|").toList(),
+                actress = movieAttributes[3].split("|").toList()
+                ,duration = movieAttributes[4].toLong(),
+                genres = movieAttributes[5].split("|").map {Genres.valueOf(value =it)},
+                imdbID = movieAttributes[6],
+                director = movieAttributes[7])
+                }
+    }
 
 
 }
@@ -40,21 +71,43 @@ class MovieStore(var movieList:List<Movie>) {
 
 
 fun main(args: Array<String>) {
-    var  movies= listOf(Movie("1","3 Idiots", LocalDate.of(2009,12,25), listOf("Comedy,Drama"),"Rajkumar Hirani",listOf("Aamir Khan","Madhavan","Sharman Joshi"),listOf("Kareena Kapoor"),170)
+
+ /*   var  movies= listOf(Movie("1","3 Idiots", LocalDate.of(2009,12,25), listOf("Comedy,Drama"),"Rajkumar Hirani",listOf("Aamir Khan","Madhavan","Sharman Joshi"),listOf("Kareena Kapoor"),170)
             ,Movie(title = "Munna Bhai M.B.B.S.",releaseDate = LocalDate.of(2003,12,19),actors = listOf("Sunil Dutt,Sanjay Dut,Arshad Warsi"),actress = listOf("Gracy Singh"),duration = 156L
             ,genres = listOf("Comedy,Drama,Musical"),imdbID = "tt0374887",director = "Rajkumar Hirani"));
+*/
+    val mutableList : MutableList<Movie> = ArrayList()
 
 
-    File("src/movie/movies.csv").readLines().drop(1).map { row->row.split(",") }
+    var moviesList=File("src/movie/movies.csv").readLines(Charsets.US_ASCII).drop(1).dropLast(1);
 
-    val movieStore=MovieStore(movies);
+    for(movie in moviesList){
+  /*          val movieAttributes=movie.split(",");
+            val movieObject=Movie(title = movieAttributes[0],
+                releaseDate = LocalDate.parse(movieAttributes[1], DateTimeFormatter.ofPattern("yyyy/MM/dd")),
+                actors = movieAttributes[2].split("|").toList(),
+                actress = movieAttributes[3].split("|").toList()
+                ,duration = movieAttributes[4].toLong(),
+                genres = movieAttributes[5].split("|").toList(),
+                imdbID = movieAttributes[6],
+                director = movieAttributes[7])
+  */
+
+        val movieObject =Movie.createMovie(movie)
+        mutableList.add(movieObject)
+
+        }
 
 
-    print(movieStore.getMoviesByRelaseYear(2003))
 
-    print(movieStore.getMoviesByGreaterTimeDuration(150))
-    print(movieStore.getMoviesByActorName("Aamir Khan"))
-    print(movieStore.getMoviesByActressName("Gracy Singh"))
+    //val movieStore=MovieStore(movies);
+    val movieStore=MovieStore(mutableList);
+
+    println(movieStore.getMoviesByRelaseYear(2003))
+
+    println(movieStore.getMoviesByGreaterTimeDuration(150))
+    println(movieStore.getMoviesByActorName("Aamir Khan"))
+    println(movieStore.getMoviesByActressName("Gracy Singh"))
 
 
 }
---
+++ src/test/MovieTest.kt
@@ -0,0 +1,14 @@
+package test
+
+import io.kotlintest.specs.StringSpec
+
+/**
+ * Created by Mahesh on 23-06-2018.
+ */
+
+
+
+class MyTests : StringSpec({
+
+
+})
